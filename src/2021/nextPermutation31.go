81076eae1906c3e791c7544e5373dadf40384086
zhangshipeng <magic_of_java@foxmail.com>
2021-09-21 22:16:21 +0800

    31

---
+++ src/2021/nextPermutation31.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums:=[]int{2,3,1,3,3}
+	nextPermutation(nums)
+	fmt.Println(nums)
+}
+
+func nextPermutation(nums []int) {
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := n - 1
+		for j >= 0 && nums[i] >= nums[j] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	reverse(nums[i+1:])
+}
+
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
+//
+//作者：LeetCode-Solution
+//链接：https://leetcode-cn.com/problems/next-permutation/solution/xia-yi-ge-pai-lie-by-leetcode-solution/
+//来源：力扣（LeetCode）
+//著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
